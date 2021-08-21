91ffa35489d0c3f8f0a869e3eda86ba5d0b3f4c9
ehdwn4019 <ehdwn7531@naver.com>
2021-08-22 02:56:38 +0900

    - Monster 수정중 - 오른쪽맵 구현중 : Cannon 생성 - Santa 구현중 - Get UI 추가 - Cannon 구현중

---
+++ CreationGame/Assets/Scripts/Objstacles/Cannon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : Obstacles
+{
+    [SerializeField]
+    Transform target;
+
+    protected override void Init()
+    {
+        base.Init();
+    }
+
+    protected override void Loop()
+    {
+        base.Loop();
+
+        LookTarget();
+    }
+
+    void LookTarget()
+    {
+        Vector3 forward = target.position - transform.position;
+        Quaternion quaternion = Quaternion.LookRotation(forward);
+        transform.rotation = Quaternion.Lerp(transform.rotation, quaternion, Time.deltaTime * 5.0f);
+    }
+}
